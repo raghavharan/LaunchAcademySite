6c1de70a15b2ee8c71eacb6c500f8036aa4d5f32
Raghav Haran <rharan5930@gmail.com>
2013-12-18 12:21:44 -0500

    Launch Academy Site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaunchAcademySite::Application.config.secret_key_base = '73950bebbfb7006db236c0c8721d1dce579666c5267cf0339089f1f667199e81c51a9fa50f2d3adbe882a833bdaf17920f0c1e3ff19aa005b40c18b332ee6995'
